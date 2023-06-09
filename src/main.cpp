fe063d37b35d37f130285c4728e9d86902520cf3
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2023-06-09 00:53:14 -0300

    Re-estructuración y adición de test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "modulos/juego/turnos/turnos.h"
 using namespace std;
 
 int main(){
-    
+    cout << "Holaaa";
     return 0;
 }
---
+++ src/modulos/juego/turnos/turnos.h
@@ -0,0 +1,11 @@
+#ifndef TURNOS_H
+#define TURNOS_H
+
+#include <iostream>
+using namespace std;
+
+void asignarTurno(){
+    cout <<"Asignar turno";
+}
+
+#endif
