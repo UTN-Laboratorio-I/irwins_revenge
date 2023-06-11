ffbbdf20168061b230947bf66c8e43040f8c7d87
FedericoECaceres <fedcaceresz@gmail.com>
2023-06-10 22:31:43 -0300

    ultimos cambios de estadisticas

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-
+    cout << "Hola pablo"; 
 
     return 0;
 }
--- src/modulos/estadisticas/estadisticas.cpp
+++ src/modulos/estadisticas/estadisticas.cpp
@@ -11,6 +11,10 @@ int main(){
     const int COL = 2;
     int m [FILAS][COL] = {{},{}};
     string vec_hito [5] = {};
+    int acuJ1 = 0;
+    int acuJ2 = 0;
+    string ganador;
+    int puntosGanador;
 
     vec_hito [0] = "Estatuilla";
     vec_hito [1] = "Estatuilla ++";
@@ -22,7 +26,9 @@ int main(){
 
     for(int i=0; i<=6; i++){
         vec_jugador1 [i] = i+1;
+        acuJ1 += vec_jugador1[i];
         vec_jugador2 [i] = i+2;
+        acuJ2 += vec_jugador2[i];
     }
 
     /*for(int i=0; i<FILAS; i++)
@@ -36,17 +42,33 @@ int main(){
         }
     }*/
 
+    cout << "IRWIN'S REVENGE - FASE FINAL" << endl;
+    cout << "------------------------------------------------------------------------" << endl << endl;
+    cout << "HITO" << "\t" << "\t" << "JUGADOR 1" << "\t" << "JUGARDOR 2" << endl;
+
     for (int i=0; i< FILAS; i++)
     {
         for (int  j=0; j < COL - 1; j++)
         {
-            cout << vec_hito[i] <<"\t"<< "J1: ";
-            cout << vec_jugador1 [i] << "\t";
-            cout << "J2: ";
-            cout << vec_jugador2 [i];
+            cout << vec_hito[i] << "\t";
+            cout << vec_jugador1 [i] << ": PDV" "\t" << "\t";
+            cout << vec_jugador2 [i] << ": PDV";
         }
         cout << "\n";
     }
+    cout << endl;
+    cout << "------------------------------------------------------------------------" << endl;
+    cout << "TOTAL" << "\t" << "\t" << acuJ1 << ": PDV"<< "\t" << "\t" << acuJ2 << ": PDV" << endl;
+
+    if(acuJ1 > acuJ2){
+        ganador = "JUGADOR 1";
+        puntosGanador = acuJ1;
+    }else{
+        ganador = "JUGADOR 2";
+        puntosGanador = acuJ2;
+    }
+
+    cout << "GANADOR: " << ganador << " con " << puntosGanador << " putnos de victoria.";
     
     
     getch ();
