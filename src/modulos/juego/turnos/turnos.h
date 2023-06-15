15c7d82cc9db8efddda9762820116de400c103fd
sasha <sashafranco7@gmail.com>
2023-06-14 21:24:55 -0300

    ultima revision

--- src/modulos/juego/turnos/turnos.h
+++ src/modulos/juego/turnos/turnos.h
@@ -4,12 +4,51 @@
 #include <iostream>
 using namespace std;
 
-void asignarTurno()
+// esta funcion permite que los jugadores elijan estatuillas
+void asignarTurno(bool turno_jugador1, bool turno_jugador2, int TAM, string jugador1, string jugador2,
+                  int &estatuilla_seleccionada_j1, int &estatuilla_seleccionada_j2)
 {
+    int i;
 
+    if (turno_jugador1 == true && turno_jugador2 == false)
+    {
 
-    
-    cout << "Asignar turno";
+        for (i = 0; i < TAM; i++)
+        {
+            cout << num_estatuilla[i] << " - " << estatuillas[i] << endl;
+        }
+        cout << jugador1 << " escoja una estatuilla" << endl;
+        cin >> estatuilla_seleccionada_j1;
+        cout << endl;
+
+        for (i = 0; i < TAM; i++)
+        {
+            cout << num_estatuilla[i] << " - " << estatuillas[i] << endl;
+        }
+        cout << jugador2 << " escoja una estatuilla" << endl;
+        cin >> estatuilla_seleccionada_j2;
+        cout << endl;
+    }
+    else
+    {
+        if (turno_jugador2 == true && turno_jugador1 == false)
+        {
+
+            for (i = 0; i < TAM; i++)
+            {
+                cout << num_estatuilla[i] << " - " << estatuillas[i] << endl;
+            }
+            cout << jugador2 << " escoja una estatuilla" << endl;
+            cin >> estatuilla_seleccionada_j2;
+            cout << endl;
+
+            for (i = 0; i < TAM; i++)
+            {
+                cout << num_estatuilla[i] << " - " << estatuillas[i] << endl;
+            }
+            cout << jugador1 << " escoja una estatuilla" << endl;
+            cin >> estatuilla_seleccionada_j1;
+            cout << endl;
+        }
+    }
 }
-
-#endif
