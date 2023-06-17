054d29eca6dbe09a34b5e3b9c3f10679f41ad102
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2023-06-16 21:10:53 -0300

    Add función para eliminar estatuillas ya ganadas

--- src/modulos/juego/estatuillas/estatuillas.h
+++ src/modulos/juego/estatuillas/estatuillas.h
@@ -36,34 +36,56 @@ using namespace std;
         //Retornamos la estatuilla que fue seleccionada:
         return listado_seleccion_temp[estatuilla_seleccionada];
     }
-    
-    //Guardamos la estatuilla y la eliminamos del array gral.
-    void guardarYEliminarEstatuilla(){
 
+    void asignarEstatuillaAJugador(int pos, string estatuilla){
+
+    }
+    //Guardamos la estatuilla y la eliminamos del array gral.
+    void eliminarEstatuilla(string estatuilla, string estatuillas_disponibles[]){
+            // for(int i = 0; i<5; i++){
+        //         cout << estatuillas_disponibles[i] <<endl;
+
+        // }
+        int i = 0, max=5;
+        bool reemplazada = 0;
+        for(i; i<max; i++){
+            if(estatuillas_disponibles[i]==estatuilla){
+                estatuillas_disponibles[i] = estatuillas_disponibles[i+1];
+                reemplazada=true;
+            }else if(reemplazada==1 && i !=max-1){
+                estatuillas_disponibles[i]=estatuillas_disponibles[i+1];
+            }
+            estatuillas_disponibles[i] = (i == max-1)? "" : estatuillas_disponibles[i];
+        }
+        cout << endl<<endl;
+        // for(int i = 0; i<5; i++){
+        // cout << estatuillas_disponibles[i]<<endl;
+        // }
     }
 
 //Asignamos la estatuilla al jugador que la ganó:
-    void asignarEstatuillaAdquirida(string estatuilla, string jugadores[], string jug){
+    void asignarEstatuillaAdquirida(string estatuilla, string jugadores[], string jug, string estatuillas_disponibles[]){
         int pos;
         for(int i =0; i<2;i++){                 //Buscamos la posición del jugador en el array de jug.
             if(jugadores[i]==jug){
                 pos=i;                          //Asignamos la posición a la variable.
             }
         }
-
-        switch(pos){                            
-            case 0:
-                break;
-            case 1:
-                break;
-            default:
-                break;
-        }
+    switch(pos){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            break;
+    }
+    //Eliminamos la estatuilla del array de disponibles.
+    eliminarEstatuilla(estatuilla,estatuillas_disponibles);
     }
 
 //VARIABLES PARA MAIN.CPP:
 //CANGREJO:
-    void obtener_cangrejo(int tirada_dados_10_caras[], int turno, string turnos[], string jugadores[]){
+    void obtener_cangrejo(int tirada_dados_10_caras[], int turno, string turnos[], string jugadores[], string estatuillas_disponibles[]){
         string estatuilla = "CANGREJO";
         bool par=0;
         bool impar=0;
@@ -77,6 +99,6 @@ using namespace std;
         }
         if(par && impar){
             string jug = turnos[turno];
-            asignarEstatuillaAdquirida(estatuilla, jugadores, jug);
+            asignarEstatuillaAdquirida(estatuilla, jugadores, jug, estatuillas_disponibles);
         }
     }
